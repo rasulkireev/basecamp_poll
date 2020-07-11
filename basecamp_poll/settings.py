dff44f9fc7b2b01584293ac4b52dbc973de3c29d
Rasul Kireev <me@rasulkireev.com>
2020-07-11 02:36:34 -0400

    basecamp signup auth complete

--- basecamp_poll/settings.py
+++ basecamp_poll/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.basecamp',
+    "sslserver",
     
 ]
 
@@ -61,7 +62,7 @@ ROOT_URLCONF = 'basecamp_poll.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
